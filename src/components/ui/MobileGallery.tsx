5cf5ea66b4b4680205b91f692c5fff6b88c8db5a
Rummiya <rumibashirova@gmail.com>
2025-03-27 00:44:54 +0600

    modified mobile gallery

--- src/components/ui/MobileGallery.tsx
+++ src/components/ui/MobileGallery.tsx
@@ -1,57 +1,79 @@
-import { GalleryCategory } from "../pages/homePages/home/Portfolio";
-import scss from "../pages/homePages/home/Portfolio.module.scss";
-import Image from "next/image";
+import Image from 'next/image';
+import { GalleryCategory } from '../pages/homePages/home/Portfolio';
+import scss from '../pages/homePages/home/Portfolio.module.scss';
 
 interface MobileGalleryProps {
-  activeCategory: GalleryCategory;
+	activeCategory: GalleryCategory;
 }
 
 export const MobileGallery = ({ activeCategory }: MobileGalleryProps) => (
-  <div className={scss.mobileImages}>
-    <div className={scss.mobileHead}>
-      <div className={scss.img}>
-        <Image
-          src={activeCategory.images[0][0]}
-          alt="Portfolio 1"
-          fill
-          style={{ objectFit: "cover" }}
-          sizes="357px" 
-          priority
-        />
-      </div>
-      {activeCategory.images[0][1].map((image, idx) => (
-        <div key={idx} className={scss.img}>
-          <Image
-            src={image}
-            alt={`Portfolio ${idx + 2}`}
-            fill
-            style={{ objectFit: "cover" }}
-            sizes="357px" 
-          />
-        </div>
-      ))}
-    </div>
-    <div className={scss.mobileFoot}>
-      {activeCategory.images[1][0].map((image, idx) => (
-        <div key={idx} className={scss.img4}>
-          <Image
-            src={image}
-            alt={`Portfolio ${idx + 4}`}
-            fill
-            style={{ objectFit: "cover" }}
-            sizes="169px"
-          />
-        </div>
-      ))}
-      <div className={scss.img}>
-        <Image
-          src={activeCategory.images[1][1]}
-          alt="Portfolio 8"
-          fill
-          style={{ objectFit: "cover" }}
-          sizes="357px" 
-        />
-      </div>
-    </div>
-  </div>
+	<div className={scss.mobileImages}>
+		<div className={scss.mobileHead}>
+			<div className={scss.threeImgs}>
+				<div className={scss.img}>
+					<Image
+						src={activeCategory.images[0][0]}
+						alt='Portfolio 1'
+						fill
+						style={{ objectFit: 'cover' }}
+						sizes='357px'
+						priority
+					/>
+				</div>
+				{activeCategory.images[0][1].map((image, idx) => (
+					<div key={idx} className={scss.img}>
+						<Image
+							src={image}
+							alt={`Portfolio ${idx + 2}`}
+							fill
+							style={{ objectFit: 'cover' }}
+							sizes='357px'
+						/>
+					</div>
+				))}
+			</div>
+			<div className={scss.img}>
+				<Image
+					src={activeCategory.images[1][0][0]}
+					alt={`Portfolio 2`}
+					fill
+					style={{ objectFit: 'cover' }}
+					sizes='357px'
+				/>
+			</div>
+			<div className={scss.footThreeImgs}>
+				{activeCategory.images[1][0].slice(1).map((image, idx) => (
+					<div key={idx} className={scss.img4}>
+						<Image
+							src={image}
+							alt={`Portfolio ${idx + 4}`}
+							fill
+							style={{ objectFit: 'cover' }}
+							sizes='169px'
+						/>
+					</div>
+				))}
+			</div>
+		</div>
+		<div className={scss.mobileFoot}>
+			<div className={scss.img4}>
+				<Image
+					src={activeCategory.images[0][0]}
+					alt={`Portfolio 4`}
+					fill
+					style={{ objectFit: 'cover' }}
+					sizes='169px'
+				/>
+			</div>
+			<div className={scss.img}>
+				<Image
+					src={activeCategory.images[1][1]}
+					alt='Portfolio 8'
+					fill
+					style={{ objectFit: 'cover' }}
+					sizes='357px'
+				/>
+			</div>
+		</div>
+	</div>
 );
