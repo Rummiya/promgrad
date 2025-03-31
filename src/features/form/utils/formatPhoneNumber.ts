2ec5f99a1fac3e5a4433eba28c35b24972983f9e
Rummiya <rumibashirova@gmail.com>
2025-03-31 11:09:00 +0600

    bug fixed

--- src/features/form/utils/formatPhoneNumber.ts
+++ src/features/form/utils/formatPhoneNumber.ts
@@ -1,8 +1,8 @@
 export const formatPhoneNumber = (value: string) => {
-	let val = value.replace(/\D/g, '').substring(0, 12);
+	const replaced = value.replace(/\D/g, '').substring(0, 12);
 	let formatted = '+996 ';
-	if (val.length > 3) formatted += val.slice(3, 6);
-	if (val.length > 6) formatted += ' ' + val.slice(6, 9);
-	if (val.length > 9) formatted += ' ' + val.slice(9, 12);
+	if (replaced.length > 3) formatted += replaced.slice(3, 6);
+	if (replaced.length > 6) formatted += ' ' + replaced.slice(6, 9);
+	if (replaced.length > 9) formatted += ' ' + replaced.slice(9, 12);
 	return formatted;
 };
